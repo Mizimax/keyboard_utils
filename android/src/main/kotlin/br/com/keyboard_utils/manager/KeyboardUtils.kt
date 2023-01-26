34c47ceb1ff117e49fdb49af5fa388e62401acc6
Tammanoon <tammanoon.jomjaturong@scbtechx.io>
2023-01-26 13:02:57 +0700

    Change condition to check height

--- android/src/main/kotlin/br/com/keyboard_utils/manager/KeyboardUtils.kt
+++ android/src/main/kotlin/br/com/keyboard_utils/manager/KeyboardUtils.kt
@@ -106,7 +106,7 @@ class KeyboardUtilsImpl(private val activity: Activity) : KeyboardUtils {
                 var heightDiff = screenHeight - (r.bottom - r.top)
 
                 // 0.15 ratio is perhaps enough to determine keypad height.
-                if (heightDiff > 100) { 
+                if (heightDiff > screenHeight * 0.15) { 
                     keyboardOpenedEvent(heightDiff)
                 }
                 else {
